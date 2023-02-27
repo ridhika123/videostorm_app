faa5010c9d4eeb33b08e9ef266493bc54c81e78a
ridhika123 <37682124+ridhika123@users.noreply.github.com>
2023-02-27 12:22:30 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ with st.container():
 # Loading in data 
 main_category = ('','Household Demographic', 'Insurance Behavior', 'Alcohol Behavior', 'Apparel and Jewelry Behavior', 'Automotive Behavior', 
                 'Commuting Behavior', 'Video Consumption Behavior', 'Environment-related Behavior', 'Financial Behavior', 'Food and Beverages Behavior')
-df = pd.read_excel('household_demo.xlsx')
-# household_demo = pd.read_excel('data.xlsx', sheet_name = 'household_demo')
+# df = pd.read_excel('household_demo.xlsx')
+household_demo = pd.read_excel('data.xlsx', sheet_name = 'household_demo')
 # insurance_behavior = pd.read_excel('data.xlsx', sheet_name = 'insurance_behavior')
 # alcohol_behavior = pd.read_csv('data.csv', sheet_name = 'alcohol_behavior')
 # apparel_and_jewelry = pd.read_csv('data.csv', sheet_name = 'apparel_and_jewelry')
